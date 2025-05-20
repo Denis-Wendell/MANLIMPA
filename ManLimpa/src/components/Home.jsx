a3c33a17c3f4de1013515fc802a6b00d78bc7311
Francisco Vinicius <franciscoviniciusvb@hotmail.com>
2025-05-20 16:40:45 -0400

    Componente "Impacto Ambiental"

--- ManLimpa/src/components/Home.jsx
+++ ManLimpa/src/components/Home.jsx
@@ -41,7 +41,7 @@ const ImageCarousel = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows: true,
+    arrows: false,
     responsive: [
       {
         breakpoint: 768,
@@ -73,60 +73,71 @@ const ImageCarousel = () => {
     display: "flex",
     justifyContent: "center",
     gap: "200px",
-    marginTop: "20px",
+    marginTop: "5px",
     flexWrap: "wrap",
-    padding: "10px 10px 0",
+    padding: "10px 0 10px 10px",
     backgroundColor: "#2e2e2e",
     borderRadius: "10px",
     color: "white",
   }}
 >
   {/* Componente de Informações */}
-  
-  <div style={{ textAlign: "left", minWidth: "180px" }}>
-   <a href="../pages/coleta">Ecopontos</a>
-    <p style={{ margin: "10px 0", fontSize: "14px" }}>
+
+  <div style={{ textAlign: "left", minWidth: "100px" }}>
+    <a href="../pages/coleta">Ecopontos</a>
+    <p style={{ margin: "5px 0", fontSize: "14px" }}>
       Encontre pontos de coleta
     </p>
   </div>
 
-  <div style={{ textAlign: "left", minWidth: "180px" }}>
+  <div style={{ textAlign: "left", minWidth: "10px" }}>
     <a href="../pages/denuncia">Central de Denúncias</a>
-    <p style={{ margin: "10px 0", fontSize: "14px" }}>
+    <p style={{ margin: "5px 0", fontSize: "14px" }}>
       Realize denúncias de descarte indevidos
     </p>
   </div>
 
   <div style={{ textAlign: "left", minWidth: "180px" }}>
     <a href="../pages/conscientizacao">Conscientização</a>
-    <p style={{ margin: "10px 0", fontSize: "14px" }}>
+    <p style={{ margin: "5px 0", fontSize: "14px" }}>
       Saiba como e onde descartar seu lixo
     </p>
   </div>
 
   <div style={{ textAlign: "left", minWidth: "180px" }}>
     <a href="../pages/residuos">Resíduos</a>
-    <p style={{ margin: "10px 0", fontSize: "14px" }}>
+    <p style={{ margin: "5px 0", fontSize: "14px" }}>
       Conheça o que pode ou não ser lixo
     </p>
   </div>
 </div>
 
-<p style={{ textAlign: "center", margin: "20px 0", fontSize: "18px", fontWeight: "bold" }}>Mapa de Ecopontos</p>
+{/* Componente de Impacto Ambiental */}
 
-  {/* Mapa de Localização */}
-
-  <div className="w-full h-[400px] rounded-lg overflow-hidden shadow-lg">
-  <iframe
-    title="Mapa de Ecopontos"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127493.96627362698!2d-60.04950611539125!3d-3.0446589071499797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c1bc8b37647b7%3A0x2b485c9ff765a9cc!2sManaus%2C%20AM!5e0!3m2!1spt-BR!2sbr!4v1747682622768!5m2!1spt-BR!2sbr"
-    width="100%"
-    height="350px"
-    textAlign
-    allowFullScreen=""
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade"
-  ></iframe>
+  <div
+  style={{
+    marginTop: "20px",
+    padding: "20px",
+    backgroundColor: "#2e2e2e",
+    borderRadius: "10px",
+    color: "white",
+    textAlign: "center",
+  }}
+>
+  <h2 style={{ fontSize: "30px", marginBottom: "1px", fontWeight: "bold" }}>
+    Impacto Positivo em Manaus 🌱
+  </h2>
+  <br />
+  <p style={{ fontSize: "16px", marginBottom: "10px" }}>
+    ♻️ Mais de <strong>1.200 toneladas</strong> de resíduos recicláveis coletados em 2024
+  </p>
+  <p style={{ fontSize: "16px", marginBottom: "10px" }}>
+    🌍 Redução estimada de <strong>3.500 toneladas de CO₂</strong> na atmosfera
+  </p>
+  <p style={{ fontSize: "16px", marginBottom: "10px" }}>
+    ♻️ Mais de <strong>1.000 toneladas</strong> de resíduos recicláveis coletados em 2023
+    💚 Mais de <strong>200 ecopontos</strong> ativos ajudando a construir uma cidade mais sustentável!
+  </p>
 </div>
 
     </div>
