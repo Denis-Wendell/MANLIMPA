a6aae413515a0147e8be5ca6e127d336e033c3b4
Francisco Vinicius <106833771+fviniciusb@users.noreply.github.com>
2025-05-20 13:48:57 -0400

    removido as setas no carrossel

--- ManLimpa/src/components/Home.jsx
+++ ManLimpa/src/components/Home.jsx
@@ -41,7 +41,7 @@ const ImageCarousel = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows: true,
+    arrows: false,
     responsive: [
       {
         breakpoint: 768,
