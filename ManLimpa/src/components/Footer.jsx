f3873af642724a1f20f21865ed1e672e893d4543
Denis Wendell <do72831@gmail.com>
2025-05-25 16:12:14 -0400

    Fix Footer.css import case sensitivity

--- ManLimpa/src/components/Footer.jsx
+++ ManLimpa/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/components/footer.css';
+import '../styles/components/Footer.css';
 
 const Footer = () => {
   return (
